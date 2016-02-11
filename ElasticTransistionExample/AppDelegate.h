b6e9b30c43a7f3b322b00fad6d1b69b6603315a3
taglia <the.taglia3@gmail.com>
2016-02-11 01:14:19 +0100

    Initial Commit

---
+++ ElasticTransistionExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ElasticTransistionExample
+//
+//  Created by Tigielle on 11/02/16.
+//  Copyright © 2016 Matteo Tagliafico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ElasticTransistionExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ElasticTransistionExample
+//
+//  Created by Tigielle on 11/02/16.
+//  Copyright © 2016 Matteo Tagliafico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
